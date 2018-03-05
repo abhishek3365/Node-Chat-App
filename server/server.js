4525cce40d6076a7d49738b6f9d3a7bacec8696d
SHARMA Abhishek <abhishek.sharma3@soprasteria.com>
2018-03-05 17:16:06 +0530

    Users list in room

--- server/server.js
+++ server/server.js
@@ -4,29 +4,32 @@ const express = require ('express');
 const socketIO = require ('socket.io');
 const {generateMessage , generateLocationMessage } = require('./utils/message');
 const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 
 const publicPath = path.join(__dirname , '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer( app );
 var io = socketIO(server);
+var users = new Users();
 
 app.use ( express.static(publicPath)  );
 
 io.on('connection' , (socket) => {
+
     console.log ('New User connected');
-
-
-
 
     socket.on('join',(params , callback) => {
 
         if(!isRealString( params.name ) || !isRealString(params.room) ){
-            callback('Name and room are required');
+            return callback('Name and room are required');
         }
 
         socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id , params.name , params.room)
 
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room) );
         socket.emit('newMessage',generateMessage('Admin' , 'Welcome to the chat app'));
         socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin' , `${params.name} Joined`) );
 
@@ -47,9 +50,18 @@ io.on('connection' , (socket) => {
         io.emit('newLocationMessage' , generateLocationMessage('Admin' , coords.lat , coords.lng));
     });
 
+    socket.on('disconnect', () => {
+
+        var user = users.removeUser(socket.id);
+
+        if(user){
+            io.to(user.room).emit('updateUserList' , users.getUserList(user.room) );
+            io.to(user.room).emit('newMessage' , generateMessage('Admin' , `${user.name} has left`));
+        }
+
+    });
+
 } );
-
-
 
 server.listen(port,() => {
    console.log(`Server is up at ${port}`) 
