9006653023e799d295cab797f5e775705c826f5d
abhisheksharma <sharma.abhishek.1810@gmail.com>
2019-03-08 01:55:17 +0530

    test

--- public/js/chat.js
+++ public/js/chat.js
@@ -76,7 +76,7 @@ socket.on('newImage' , function(message){
 
     var formattedTime = moment(message.createdAt).format('h:mm a');
 
-    var template = jQuery('#message-template').html();
+    var template = jQuery('#image-template').html();
     var html = Mustache.render(template , {
         image : message.text,
         from : message.from,
